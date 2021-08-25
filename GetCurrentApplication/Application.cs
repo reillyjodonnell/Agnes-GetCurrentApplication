ffd8ff60f007fc63222773ddd1eac4fa57b3cb5e
Reilly <Reillyodie@gmail.com>
2021-08-25 18:51:45 -0400

    Application now console logs the total seconds spent on each application and toggles between them - pausing as they are not active in the foreground.

--- GetCurrentApplication/Application.cs
+++ GetCurrentApplication/Application.cs
@@ -1,24 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
+
 
 namespace GetCurrentApplication
 {
-    class Application
+    public class Application
     {
         //Has a date
-        private DateTime date;
+        private string date;
         //A time
-        private DateTime timeStarted;
-        private DateTime timeEnded;
+        private TimeSpan timeStarted;
+        private TimeSpan timeEnded;
         //A string (name of the application)
         private string nameOfApplication;
         //Amount of time
         private double timeSpentOnApplication;
 
+        //the timer
+        private Stopwatch stop = Stopwatch.StartNew();
+
+        
 
 
-        ///functions
+        // When I create an application I want several things:
+        // 1. To immediately start the timer
+        // 2. To create variable instances
+        public Application(string applicationName)
+        {
+            nameOfApplication = applicationName;
+            date = DateTime.Now.ToString("MM-yyyy-dd");
+            timeStarted = DateTime.Now.TimeOfDay;      
+        }
+
+        public void PauseStopwatch()
+        {
+            stop.Stop();
+        }
+
+        public void ResumeStopwatch()
+        {
+            stop.Start();
+        }
+
+        public string consoleTimeElapsed()
+        {
+            return stop.Elapsed.ToString();
+        }
+        
+
+        public void ViewTimeOfCurrentApplication( )
+        {
+            Console.WriteLine($"{nameOfApplication} has been running for {consoleTimeElapsed()} seconds");
+        }
+
+
+
+
+
+        /*
+        ///getter and setter methods
         public DateTime getDate()
         {
             return date;
@@ -28,17 +69,17 @@ namespace GetCurrentApplication
             date = passedDate;
         }
 
+
         public DateTime getTimeStarted()
         {
             return timeStarted;
         }
-
-
         public void setTimeStarted(DateTime time)
         {
             timeStarted = time;
         }
-        public DateTime getTimeEnded( )
+
+        public DateTime getTimeEnded()
         {
             return timeEnded;
         }
@@ -47,6 +88,7 @@ namespace GetCurrentApplication
             timeEnded = time;
         }
 
+        */
         public string getNameOfApplication()
         {
             return nameOfApplication;
@@ -55,6 +97,8 @@ namespace GetCurrentApplication
         {
             nameOfApplication = name;
         }
+        /*
+
 
         public double getTimeSpentOnApplication()
         {
@@ -64,5 +108,51 @@ namespace GetCurrentApplication
         {
             timeSpentOnApplication = time;
         }
+        /*
+
+
+
+
+
+
+        /*
+        // Set a variable to the Documents path.
+        string docPath =
+          Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        //Pause any existing timers
+        pauseAllRunningTimers();
+
+            if (listOfApplicationNames.Contains(currentApplicationName) == false)
+            {
+                Console.WriteLine("Detected new application running!");
+
+
+                
+
+                /// Append text to an existing file named "WriteLines.txt".
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CurrentApplication.txt"), true))
+                {
+                    outputFile.WriteLine(applicationName, GetTitleOfActiveApplication().StartTime);
+                    
+                }
+    uniqueApplicationNames.Add(applicationName);
+
+                //Initialize a timer to track how long the current application is being used for
+                createTimerForCurrentApplication(applicationName);
+    //Begin the timer
+    startTimingCurrentApplication(applicationName);
+
+
+
+
+    } else
+    {
+        //The item has already previously existed and the timer should just continue
+        startTimingCurrentApplication(applicationName);
+                }
+             //Console.WriteLine(uniqueApplicationNames.Count);
+            }
+        */
     }
 }
--- GetCurrentApplication/Worker.cs
+++ GetCurrentApplication/Worker.cs
@@ -29,13 +29,15 @@ namespace GetCurrentApplication
         static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
 
-        static Dictionary<string, Stopwatch> stop = new Dictionary<string, Stopwatch>();
+        //Create a dynamic array of Applications
+        //List<Application> listOfAllRunningApplications = new List<Application>();
 
-        
 
-        static string currentTask = "";
+        //static Dictionary<string, Stopwatch> stop = new Dictionary<string, Stopwatch>();
 
-        static List<string> uniqueApplicationNames = new List<string>();
+
+
+
 
         /*
         public static void initStopwatch()
@@ -73,17 +75,7 @@ namespace GetCurrentApplication
                 Console.WriteLine($"Gazed at {item.Key} for {item.Value.Elapsed.TotalSeconds} seconds");
             }
         }
-
-        static void getTimeUserOpenedCurrentApplication(string currentApplication)
-        {
-
-        }
-
-        static void ViewTimeOfCurrentApplication(string currentApplication)
-        {
-            Console.WriteLine($"{currentApplication} has been running for {stop[currentApplication]} seconds");
-        }
-
+       
         static string findCurrentApplicationName()
         {
             string applicationTitle = GetTitleOfActiveApplication().ProcessName;
@@ -172,103 +164,181 @@ namespace GetCurrentApplication
         }
 
 
-        private void countActivityOnActiveWindow()
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Create a list of instances of the Application class
+        List<Application> listOfAllRunningApplications = new List<Application>();
+
+        Application activeApplication;
+
+        List<string> namesOfAllRunningApplications = new List<string>();
+
+        static Dictionary<string, Stopwatch> stop = new Dictionary<string, Stopwatch>();
+
+        //static Dictionary<string, Application> collectionOfApplications = new Dictionary<string, Application>
+
+  
+        public string getTheCurrentApplicationName()
         {
-            var title = GetTitleOfActiveApplication().MainWindowTitle;
-            if(title != currentTask )
-            {
-                //Set the currentTask as this
-                currentTask = title;
-                StartStopwatch(title);
+            string currentApplicationName = GetTitleOfActiveApplication().ProcessName;
 
-                
-
-
-            }
+            return currentApplicationName;
+            
         }
 
-        public static void pauseAllRunningTimers()
+       
+
+
+        public void pauseAllTimersExceptActiveApplication()
         {
-            foreach(var item in stop)
+            string activeApplication = getTheCurrentApplicationName();
+
+            foreach(var list in listOfAllRunningApplications)
             {
-                PauseStopwatch(item.Key);
-            }
-
-        }
-
-
-
-
-        //This function gets the unique applications and calls subscriber
-        public void getTheUniqueApplications()
-        {
-            /*
-             * CHEAT SHEET *
-            //Both Get the executable path for the application running  *BELOW*
-            //Console.WriteLine( GetTitleOfActiveApplication().MainModule.FileName);
-
-            //Gets the common name for the application like "Spotify" "Chrome" etc..
-            //Console.WriteLine(GetTitleOfActiveApplication().ProcessName);
-
-            //Gets the time the user started the applciation (even works on specific tabs on Chrome)
-            //Console.WriteLine(GetTitleOfActiveApplication().StartTime);
-            */
-
-
-            //Gets the name of the tab for the active window along with the process name
-            //Console.WriteLine(GetTitleOfActiveApplication().MainWindowTitle);
-
-
-            /*
-            //Console.WriteLine(GetCaptionOfActiveWindow());
-            // _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            */
-
-            //For dynamic sizing it is easier to use a list compared to an array bc you don't have to know size
-            ///Declare a list of applications to store the unique processes.
-           
- 
-            ///Get the application name
-            string applicationName = GetTitleOfActiveApplication().ProcessName;
-
-            ///Write to a file to test this
-            // Set a variable to the Documents path.
-            string docPath =
-          Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            //Pause any existing timers
-            pauseAllRunningTimers();
-
-            if (uniqueApplicationNames.Contains(applicationName) == false)
-            {
-                Console.WriteLine("Detected new application running!");
-
-
-                
-
-                /// Append text to an existing file named "WriteLines.txt".
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CurrentApplication.txt"), true))
+                if (list.getNameOfApplication() != activeApplication)
                 {
-                    outputFile.WriteLine(applicationName, GetTitleOfActiveApplication().StartTime);
-                    
+                    list.PauseStopwatch();
                 }
-                uniqueApplicationNames.Add(applicationName);
+                else 
+                { 
+                    list.ResumeStopwatch();
+                    list.ViewTimeOfCurrentApplication();
 
-                //Initialize a timer to track how long the current application is being used for
-                createTimerForCurrentApplication(applicationName);
-                //Begin the timer
-                startTimingCurrentApplication(applicationName);
+                }
+            }
+           
+        }
 
 
+        public bool checkIfCurrentApplicationHasRunBefore()
+        {
+            string currentApplication = getTheCurrentApplicationName();
+
+            if (namesOfAllRunningApplications.Contains(currentApplication))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void checkActiveApplication()
+        {
+            pauseAllTimersExceptActiveApplication();
+            Console.WriteLine($"{getTheCurrentApplicationName()} is the active screen");
+
+        }
+
+
+
+
+        public void setCurrentApplicationToList()
+        {
+            if(checkIfCurrentApplicationHasRunBefore() == true)
+            {
+                checkActiveApplication();
+            }
+            else if(checkIfCurrentApplicationHasRunBefore() == false)
+            {
+                //stop all timers
+                pauseTheTimer(activeApplication);
+
+                Application currentRunningApplication = createInstanceOfApplication();
 
                 
-            } else
-            {
-                //The item has already previously existed and the timer should just continue
-                startTimingCurrentApplication(applicationName);
+
+                activeApplication = currentRunningApplication; 
+                
+                addInstatiatedObjectToApplicationList(currentRunningApplication);
+
+                addInstatiatedObjectToStringList();
             }
-            //Console.WriteLine(uniqueApplicationNames.Count);
         }
+
+        public void pauseTheTimer(Application currentApplication)
+        {
+            if (currentApplication != null)
+            {
+                currentApplication.PauseStopwatch();
+
+            }
+            else return;
+        }
+
+       
+
+ 
+        public Application createInstanceOfApplication()
+        {
+            string nameOfApplication = getTheCurrentApplicationName();
+
+            Application application = new Application(nameOfApplication);
+
+            return application;
+            
+        }
+
+        public void addInstatiatedObjectToApplicationList(Application activeApplication)
+        {
+            listOfAllRunningApplications.Add(activeApplication);
+        }
+
+        public void addInstatiatedObjectToStringList()
+        {
+            string currentApplication = getTheCurrentApplicationName();
+            namesOfAllRunningApplications.Add(currentApplication);
+        }
+
+        public void lookForApplications()
+        {
+            setCurrentApplicationToList();
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
      
 
         public static void writeAllDataToFile()
@@ -294,12 +364,15 @@ namespace GetCurrentApplication
 
                 command.ExecuteNonQuery();
 
-                foreach(var item in stop)
+                foreach (var item in stop)
                 {
                     string sql2 = $"INSERT into userActivity (application, time) values('{item.Key}', {item.Value.Elapsed.TotalSeconds} )";
                     SqliteCommand command2 = new SqliteCommand(sql2, connection);
                     command2.ExecuteNonQuery();
+
                 }
+
+
                 connection.Close();
             }
 
@@ -326,15 +399,17 @@ namespace GetCurrentApplication
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+
+            Console.WriteLine("Test");
             
             //
             while (!stoppingToken.IsCancellationRequested)
             {
                 //Call the method to get the unqiue applications and record the amount of time spent on the applications
                 //Only problem is that this is being called repeatedly every second. So the function never truly stores anything in it's scope.
-                getTheUniqueApplications();
+                //getTheUniqueApplications();
 
-                ViewTimesOfAllRunningApplications();
+                lookForApplications();
 
 
                 await Task.Delay(1000, stoppingToken);
